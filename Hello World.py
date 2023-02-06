fc59bcdee2bd583dd802bf55d3592cec6229bbf9
Mateus Quadros <99994369+mateusfq@users.noreply.github.com>
2023-02-05 22:58:20 -0300

    Added Hello World.py
    My first program using Python, while doing the LinkedIn Learning Python course by Joe Marini

---
+++ Hello World.py
@@ -0,0 +1,12 @@
+#
+# Example file for HelloWorld
+# LinkedIn Learning Python course by Joe Marini
+#
+
+def main():
+    print("Hello World")
+    name = input("What is your name? ")
+    print("Hello,", name, "\nNice to meet you!")
+
+if __name__ =="__main__":
+    main()
